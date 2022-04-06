9a1cfdff800ec5d35745fb48de8063af689db6ac
Xavier Dunkley <xavierxv18@gmail.com>
2022-04-05 22:13:57 -0400

    commit tree 10a5f1ef57ecdc082a6f47a523cfd662bc333520

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for(let i = 0; i<array.length; i++){
+    for(let j =i+1; j<=array.length-1; j++){
+      const arraySum = array[i]+array[j]
+      console.log(array[i])
+      console.log(array[j])
+      if(arraySum===target){
+        return true
+      }
+    }
+  }
+  return false
 }
 
 /* 
   Write the Big O time complexity of your function here
+  Big O time complexity for the function is O(n^2)
 */
 
 /* 
   Add your pseudocode here
+  Take an element in the array and add it to 
+  every other element in the array including itself.
+  Then compare it to the target. 
 */
 
 /*
   Add written explanation of your solution here
+  Everytime it runs through it will compare it to the target.
+  If they are the same, return true.
+  If not return false. 
 */
 
 // You can run `node index.js` to view these console logs
